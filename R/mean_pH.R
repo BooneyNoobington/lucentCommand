fd92447d015c4bb8572e1f04bc5939407b1d2982
Markus Roth <markusroth86@gmail.com>
2022-10-25 23:39:49 +0200

    Testing mean pH calculation.

---
+++ R/mean_pH.R
@@ -0,0 +1,47 @@
+# Other code needed to complete this function.
+source("./R/helpers.r")  # Functions to solve two to three liners.
+source("./R/sql_interop.r")  # Talk to the data base.
+ready_packages(c("yaml"))
+
+
+# Calculate mean of arbitrary inputs.
+args = commandArgs(trailingOnly = TRUE)
+
+# Interpret the arguments.
+sample.id <- args[1]
+procedure.id <- args[2]
+
+
+# Connect to the data base.
+sql_connection <- database_connection(yaml::read_yaml(file = "./conf/config.yaml"))
+
+
+# Collect all the data.
+pH.data <- fetch_data(
+    sql_connection  # Established above.
+  , create_query_string(
+        "./R/sql/mean_pH.SQL"  # Raw statement.
+        # Replace with actual ids for sample and procedure.
+      , list("{sample.id}" = sample.id, "{procedure.id}" = procedure.id)
+    )
+)
+
+print(pH.data)
+
+# Function to calculate a mean of several pHs.
+pH.mean <- function(pH.vec){
+
+    # Transform log to concentration.
+	concentration <- exp(-(pH.vec)/(log(exp(1))))
+
+	# Compute mean. Ingore "not numbers".
+	concentration.mean <- mean(concentration, na.rm = TRUE )
+
+	# Re-transform to logarithm.
+	concentration.mean.log <- -log(concentration.mean)
+
+	return(concentration.mean.log)
+}
+
+
+
