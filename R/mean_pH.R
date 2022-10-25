8b9e0d09053e5d45dba90ef9231c634b5ca95bed
Markus Roth <markusroth86@gmail.com>
2022-10-25 23:55:57 +0200

    Testing mean pH calculation.

--- R/mean_pH.R
+++ R/mean_pH.R
@@ -17,6 +17,8 @@ procedure.id <- args[2]
 if (! file.exists("./R/mean_pH.SQL")) print("Error, query file not found.")
 quit("no")  # Abort execution, don't save workspace.
 
+print(readr::read_file("./R/mean_pH.SQL"))
+
 # Connect to the data base.
 sql_connection <- database_connection(yaml::read_yaml(file = "./conf/config.yaml"))
 
