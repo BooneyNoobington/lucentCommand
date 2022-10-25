d92a4fafd2c3642e68534528d345a6c4769228db
Markus Roth <markusroth86@gmail.com>
2022-10-25 22:33:57 +0200

    Added missing import in main class.

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
+# Define sources and targets of changed files.
 source=/home/grindel/Entwicklung/lucentLIMS/
 target=/opt/lucent/
-
 data_source=/home/grindel/Entwicklung/lucentLIMS_data_dir/
 data_target=/usr/local/var/lucent/
 
+# Make sure mariadb is running.
+sudo systemctl start mariadb.service
+
+# Copy changed data to testing location.
 sudo rsync -avu --progress "$source" "$target"
 sudo rsync -avu --progress "$data_source" "$data_target"
 
+# Correct owenership.
 sudo chown lucent:lucent -R "$target"
 sudo chown lucent:lucent -R "$data_target"
 
+# Change into source directory.
 cd $source
+
+# Add new / changed files.
 git add .
 
+# Commit with a short comment.
 git commit -m "$1"
 
+# Upload to github.
 git push origin main
 
+# Change into working direcotry (return with popd?).
 pushd /opt/lucent
 
+# Launch lucent.
 sudo -u lucent ./lucent.py
