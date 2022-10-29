6acca8cf1f93af76f87c1d3e408350dd494012d4
Markus Roth <markusroth86@gmail.com>
2022-10-29 10:45:42 +0200

    Removing errors on getOptions.

--- lucent.py
+++ lucent.py
@@ -156,7 +156,7 @@ class lucentTerminal(cmd.Cmd):
             na.attachAnalysis(self)  # Call a function for that specific purpose.
         # Generic version.
         elif l["command"].lower() == "generic":
-            print(l)
+            print(l["table"])
             try:
                 na.attachRelation(self, l["table"])
             # User didn't provide a table name.
